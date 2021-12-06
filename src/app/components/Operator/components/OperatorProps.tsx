490405be515867fa4194a944e0559c341c156990
Dmitri Meshin <dmitri@blox.io>
2021-12-06 18:40:30 +0200

    Operator and validator performances

--- src/app/components/Operator/components/OperatorProps.tsx
+++ src/app/components/Operator/components/OperatorProps.tsx
@@ -4,6 +4,6 @@ export type OperatorProps = {
   params?: any,
   // eslint-disable-next-line react/no-unused-prop-types
   isLoading?: boolean,
-  // eslint-disable-next-line react/no-unused-prop-types
+  // eslint-disable-next-line react/no-unused-prop-types,no-unused-vars
   onLoadPerformances?: (periods: string[]) => void;
 };
