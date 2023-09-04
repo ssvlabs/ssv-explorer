8a7fab4c8e951764b3c4ca4dcba156b0b15f0094
Dmitri Meshin <dmitri@blox.io>
2023-09-04 16:30:58 +0300

    Make sure build works

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { Provider } from 'mobx-react';
 import { BrowserRouter } from 'react-router-dom';
 import App from '~app/App';
 import { rootStore } from '~root/stores';
 import * as serviceWorker from '~root/serviceWorker';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+root.render(
   <Provider stores={rootStore}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </Provider>,
-  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
