16383f145ccec4b76315218210a00663513d4092
Chris <krzysztof@ssvlabs.io>
2024-07-01 21:18:23 +0200

    Implementation of Network Switch and Clean up of Prater & Goerli

---
+++ src/app/common/components/NetworkSelect/NetworkSelect.tsx
@@ -0,0 +1,77 @@
+import React, { ChangeEvent } from 'react';
+import Box from '@material-ui/core/Box';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import chainService, { EChain } from '~lib/utils/ChainService';
+import NetworkIcon from '~app/common/components/NetworkIcon';
+import { useStyles } from './NeworkSelect.styles';
+import { KeyboardArrowDown } from '@material-ui/icons';
+
+const availableNetworks = [EChain.Ethereum, EChain.Holesky];
+
+const networkToConfigMap = {
+  [EChain.Ethereum]: {
+    url: 'https://explorer.ssv.network',
+    label: 'Ethereum Mainnet',
+  },
+  [EChain.Holesky]: {
+    url: 'https://holesky.explorer.ssv.network',
+    label: 'Holesky Testnet',
+  },
+};
+
+export default function NetworkSelect() {
+  const currentNetwork: EChain = chainService().getNetwork() as EChain;
+
+  const classes = useStyles();
+
+  const handleChange = (
+    event: ChangeEvent<{ name?: string; value: unknown }>,
+  ) => {
+    const urlToGo = networkToConfigMap[event.target.value as EChain].url;
+    if (urlToGo) {
+      window.open(urlToGo, '_self');
+    }
+  };
+
+  return (
+    <Box className={classes.Wrapper}>
+      <Select
+        variant="standard"
+        autoWidth
+        value={currentNetwork}
+        onChange={handleChange}
+        IconComponent={KeyboardArrowDown}
+        className={classes.Root}
+        classes={{
+          select: classes.Select,
+          icon: classes.Icon,
+        }}
+        MenuProps={{
+          anchorOrigin: {
+            vertical: 'bottom',
+            horizontal: -16,
+          },
+          transformOrigin: {
+            vertical: 'top',
+            horizontal: 'left',
+          },
+          getContentAnchorEl: null,
+          classes: {
+            paper: classes.ItemListWrapper,
+            list: classes.ItemList,
+          },
+        }}
+      >
+        {availableNetworks.map((net) => (
+          <MenuItem value={net}>
+            <div className={classes.ItemWrapper}>
+              <NetworkIcon network={net} className={classes.ItemIcon} />
+              {networkToConfigMap[net].label}
+            </div>
+          </MenuItem>
+        ))}
+      </Select>
+    </Box>
+  );
+}
